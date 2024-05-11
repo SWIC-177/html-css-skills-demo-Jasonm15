2e5f3e7c0237fe510d69010402d0ab0cc0a0ca65
Jasonm15 <156734610+Jasonm15@users.noreply.github.com>
2024-05-10 19:18:48 -0500

    Made a file called libs.js and made a function called ERRORS to display the message if the user has not filled out the name field.

---
+++ src/libs.js
@@ -0,0 +1,9 @@
+const ERRORS = [
+    {
+        id: "name",
+        msg: "please enter your full name",
+        validate(val){
+            return val.length > 6 && val.includes(" ");
+        }
+    }
+];
