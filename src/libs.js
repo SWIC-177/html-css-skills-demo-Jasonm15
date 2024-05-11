18eeff12679a32d1ebbf44adb72e9ddd84db8f7b
Jasonm15 <156734610+Jasonm15@users.noreply.github.com>
2024-05-11 17:43:15 -0500

    changed the validate on each function to a diffrent format

--- src/libs.js
+++ src/libs.js
@@ -1,32 +1,26 @@
-import validator from 'validator';
+import validator from "validator";
 
-export const ERRORS = [
-    {
-        id: "name",
-        msg: "Please enter your full name (at least two words)",
-        validate(val) {
-            return val.split(" ").length > 1 && validator.isLength(val, { min: 6 });
-        }
-    },
-    {
-        id: "email",
-        msg: "Please enter a valid email address",
-        validate(val) {
-            return validator.isEmail(val);
-        }
-    },
-    {
-        id: "phone",
-        msg: "Please enter a valid phone number",
-        validate(val) {
-            return validator.isMobilePhone(val, 'any', { strictMode: false });
-        }
-    },
-    {
-        id: "message",
-        msg: "Message must be between 10 and 100 characters",
-        validate(val) {
-            return validator.isLength(val, { min: 10, max: 100 });
-        }
-    }
+export const VALIDATIONS = [
+  {
+    fieldId: "name",
+    errorMessage: "Please enter your full name.",
+    validate: (value) => value.length > 6 && value.includes(" "),
+  },
+  {
+    fieldId: "email",
+    errorMessage: "Please enter a valid email address.",
+    validate: validator.isEmail,
+  },
+  {
+    fieldId: "phone",
+    errorMessage: "Please enter a valid phone number.",
+    validate: (value) => validator.isMobilePhone(value, "en-US"),
+  },
+  {
+    fieldId: "message",
+    errorMessage: "Please enter a message between 10 and 100 characters.",
+    validate: (value) => value.length >= 10 && value.length <= 100,
+  },
 ];
+
+
